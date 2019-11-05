5b17c9a4337b459165d1c60f6954719937442f15
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2019-11-05 12:12:33 -0700

    Color test

---
+++ Lesson 188 Color Toggle/color.js
@@ -0,0 +1,26 @@
+// var button = document.querySelector("button")
+// button.addEventListener("click", function(){
+//     if(isPurple){
+//         document.body.style.background = "white";
+//         isPurple = false;
+//     } else {
+//         document.body.style.background = "purple"
+//         isPuprle = true
+//     }
+//     // If white, make it purple
+//         // Make it purple
+//     // Else
+//         // Make it white 
+//     isPurple = !isPurple;
+// });
+
+var button = document.querySelector("button")
+var isPurple = false;
+button.addEventListener("click", function(){
+    document.body.classList.toggle("purple");
+});
+
+
+
+        document.body.style.background = "white";
+        document.body.style.background = "purple"
