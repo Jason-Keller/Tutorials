9c249a76901bf089bd1393b69a9471a0073b63b7
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2019-11-07 12:32:20 -0700

    Update scorekeeper!

--- Lessong 189 Score Keeper Project/score.js
+++ Lessong 189 Score Keeper Project/score.js
@@ -1,6 +1,9 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -9,7 +12,9 @@ var winningScore = 5;
 p1Button.addEventListener("click", function(){
     if (!gameOver){
         p1Score++;
+        console.log(p1Score, winningScore);
         if(p1Score === winningScore){
+            p1Display.classList.add("winner");
             gameOver = true;
         }
         p1Display.textContent = p1Score;
@@ -20,8 +25,32 @@ p2Button.addEventListener("click", function(){
     if(!gameOver){
         p2Score++
         if(p2Score === winningScore){
+            p2Display.classList.add("winner");
             gameOver = true;
         }
         p2Display.textContent = p2Score;
     }
 });
+
+resetButton.addEventListener("click", function(){
+    reset();
+});
+
+numInput.addEventListener("change", function(){
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
+}); 
+
+
+// This will set scores back to 0
+// The p1Display.textcontent will then set the displayed scores to 0
+// The .classList.remove removes the "winner" class from the p1/2
+// The gameOver state is set to false and the game can be played again
+
+// p1Score = 0;
+// p2Score = 0;
+// p1Display.textContent = 0;
+// p2Display.textContent = 0;
+// p1Display.classList.remove("winner");
+// p2Display.classList.remove("winner");
+// gameOver = false;
