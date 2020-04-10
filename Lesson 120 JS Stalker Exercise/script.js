a34ba901141dea478f06fd7b12cf4459ca6aacc6
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-09 20:51:54 -0500

    Update lessons

---
+++ Lesson 120 JS Stalker Exercise/script.js
@@ -0,0 +1,6 @@
+var userFirst = prompt("Please enter your first name");
+var userLast = prompt("Please enter your last name");
+var userAge = prompt("Please enter your age");
+
+console.log("Your name is " + userFirst + " " + userLast);
+console.log("and you are " + userAge + " years old");
---
+++ Lesson 122 Age Calculator/script.js
@@ -0,0 +1,4 @@
+var age = prompt("Please enter your age");
+var days = age * 365.25;
+
+alert("You are " + days + " days old");
