887dfda48564c6dc321d07c2fdbfe348e976ed1b
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-14 14:56:44 -0600

    E

---
+++ Lesson 152 Higher Order Functions Code Along/order.js
@@ -0,0 +1,9 @@
+function sing() {
+console.log("twinke twinkle...");
+console.log("how I wonder...");
+}
+
+setInterval(function(){
+    console.log("I am an anonymous function");
+    console.log("This is awesome");
+}, 2000);
