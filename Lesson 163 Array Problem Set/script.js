8749b8f2a09075b251d095515ecffbeb80fc4f7a
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-15 19:02:56 -0600

    Update to script.js

--- Lesson 163 Array Problem Set/script.js
+++ Lesson 163 Array Problem Set/script.js
@@ -1,25 +1,41 @@
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function printReverse(arr){
 for (var i = num.length -1; i >= 0; i--) {
      console.log(num[i]);
+    }
 }
 
 //isUniform()
 //write a function isUniform() which takes an array as an argument and returns true if all elements in the array are identical
-
-function isUniform (list) {
-    var firstItem = list[0];
-    for (var i = 0; i < list.length; i++){
-        if (firstItem === list[i]) {
-            console.log("True");
-        } else {
-            console.log("False");
-        };
-    };
+function isUniform (arr){
+    var first = arr[0];
+    for(var i = 1; i < arr.length; i++){
+        if(arr[i] !== first) {
+            return false;
+        }
+    }
+    return true;
 }
 
-isUniform(['a','a','a'])
+//sumArray()
+//Write a function sumArray() that accepts an array of numbers and returns the sum of all numbers in the array
+function sumArray(arr){
+    var result = 0;
+    arr.forEach(function(element){
+        result += element;
+    });
+return result;
+}
 
-function isUniform(list) {
-    var firstItem = []
+//max()
+//Write a function max() that accepts an array of numbers and returns the maximum number in the array
+function max(arr){
+    var max = arr[0];
+        for(var i = 1; i < arr.length; i++){
+            if(arr[i] < max){
+                max = arr[i];
+            }
+        }
+    return max;
 }
