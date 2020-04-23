8404021c4b9ca5b069edcb0ceabce90e64d64767
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-23 17:37:34 -0600

    Massive overhauls of the game

--- Color Picker Game/colorGame.js
+++ Color Picker Game/colorGame.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(numSquares);
+var numSquares = 6;
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -6,19 +7,53 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+        if(colors[i]){
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < squares.length; i++){
+            squares[i].style.background = colors[i];
+            squares[i].style.display = "block";
+    }
+});
 
 resetButton.addEventListener("click", function(){
     // Generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     // Pick a new random color from array
     pickedColor = pickColor();
     // Change colorDisplay to match picked color
     colorDisplay.textContent = pickedColor;
+    this.textContent = "New Colors";
+    // Will hide the cprrect button on play again
+    messageDisplay.textContent = "";
     // Change colors of squares on page
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.background = "#232323";
+    h1.style.background = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
