813315365ba4995083c79c555f13952805ce7fb0
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-26 19:51:05 -0500

    Updating!

---
+++ Lesson 211 jQuery Selecotr Exercise/exercise.js
@@ -0,0 +1,10 @@
+divs.style.color = "pink"
+
+$("div").css("background", "purple");
+
+$("div.highlight").css("width", "200px");
+
+$("#third").css("border", "2px solid orange");
+
+var divs = document.querySelector("div");
+divs.style.color = "pink"
