adb94520e8f2d1c39660558f11652afc6e280e26
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-26 20:14:48 -0500

    wew lad

--- Lesson 211 jQuery Selecotr Exercise/exercise.js
+++ Lesson 211 jQuery Selecotr Exercise/exercise.js
@@ -8,3 +8,7 @@ $("#third").css("border", "2px solid orange");
 
 var divs = document.querySelector("div");
 divs.style.color = "pink"
+
+// or
+
+$("div:first-of-type").css("color", "pink");
---
+++ Lesson 212 Text and HTML/methods.js
@@ -0,0 +1,15 @@
+$("li").text();
+$("h1").text();
+$("h1").text("New text!!!");
+
+$("ul").html(); //gets the inner HTML of the element
+$("ul").html("<li>I hacked your UL!</li>");
+
+//ATTR = Attribute
+//Retrieve the value of an attribute. Or set one.
+
+$("img").css("width", "500px");
+$("img").attr("src", "https://farm3.staticflickr.com/2418/2243463214_f32ab004af_z.jpg");
+
+$("input").attr("text");
+
