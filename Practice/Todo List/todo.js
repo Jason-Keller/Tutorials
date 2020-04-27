c94c461eae08a0542e51d5bb47fcfc83db906a0f
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-27 18:21:33 -0500

    Update

--- Practice/Todo List/todo.js
+++ Practice/Todo List/todo.js
@@ -1,3 +1,11 @@
-document.querySelector("button").addEventListener("click", function(){
-    $("li").toggleClass("selected");
+document.querySelector(".begin").addEventListener("click", function(){
+    responder();
 });
+
+
+function responder() {
+    var response = window.prompt("Please enter your name")
+    console.log("Hello, " + response + ". Shall we begin?")
+    $(".responder").text("Hello, " + response + ". Shall we begin?");
+    return response
+}
