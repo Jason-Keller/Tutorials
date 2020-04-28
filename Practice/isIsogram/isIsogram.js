974bd0d4ba07ca0aa03c9746f1e6fb887c51095d
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-04-27 20:30:25 -0500

    WHY WON'T YOU WORK

--- Practice/isIsogram/isIsogram.js
+++ Practice/isIsogram/isIsogram.js
@@ -1,13 +1,25 @@
-// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+// Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
 // isIsogram("Dermatoglyphics") == true
 // isIsogram("aba") == false
 // isIsogram("moOse") == false // -- ignore letter case
 
+// function isIsogram(word){
+//     if(word !== "string") {
+//         return "Argument should be a string, do not use letters";
+//     }
+//     if (word === ' '){}
+// }
+
 function isIsogram(word){
-    if(word !== "string") {
-        return "Argument should be a string, do not use letters";
+    var isogram = word.split("")
+    console.log(isogram);
+    for (var i = 0; i < isogram.length; i++){
+        if (isogram[i] !== isogram[i]){
+            console.log("True, this is an isogram.");
+        } else if (isogram[i] === isogram[i]) {
+            console.log("False, this is not an isogram.");
+        }
     }
-    if (word === ' '){}
 }
-    
