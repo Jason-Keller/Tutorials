df3bee41749b9f81a1fac1d2309d5b15b34084d1
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-04 09:12:18 -0500

    Update

---
+++ Lesson267MyFirstExpressApp/app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res){
+    res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
+// "/dog" => "MEOW!"
+app.get("/dog", function(res, res){
+    console.log("Someone made a request to /dog");
+    res.send("MEOW!");
+});
+
+// Tell express to listen to requests (start server)
+
+app.listen(3000, function(){
+    console.log("Server has started!")
+});
