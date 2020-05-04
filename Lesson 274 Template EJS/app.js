ead5c0340bd99c551fedb32ec08721edba29ebcb
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-04 18:24:11 -0500

    e

--- Lesson 274 Template EJS/app.js
+++ Lesson 274 Template EJS/app.js
@@ -7,13 +7,18 @@ app.listen(3000, function(){
     console.log("=============================");
 });
 
+//This will tell express to serve the CSS file in "public"
+app.use(express.static("public"));
+//Will expect files served to be EJS files
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res){
@@ -22,5 +27,5 @@ app.get("/posts", function(req, res){
         {title: "My adorable pet bunny", author: "Charlie"},
         {title: "Can you believe this pomsky?", author: "Jason"},
     ]
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
