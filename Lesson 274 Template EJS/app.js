beb1105f1af49a317845f78db8528311edf4a28d
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-04 12:55:58 -0500

    Staged

---
+++ Lesson 274 Template EJS/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.listen(3000, function(){
+    console.log("=============================");
+    console.log("Server is listening!");
+    console.log("=============================");
+});
+
+app.get("/", function(req, res){
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love.ejs");
+});
