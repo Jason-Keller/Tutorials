f65f805db60ef12e406457fa2468e108e11bf8c9
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-03 20:14:13 -0500

    Fix

---
+++ Lesson 258 Node Echo/echo.js
@@ -0,0 +1,8 @@
+function echo(str, num){
+    for(var i = 0; i < num; i++){
+        console.log(str);
+    }
+}
+
+echo("Echo!!!", 10) // Should print out Echo!!! 10 times
+echo("Jason!!!", 3) // Should print out Jason!!! 10 times
