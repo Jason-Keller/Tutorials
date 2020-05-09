ff6b987cc5b3e398136e447ad16e69a86fa1c07a
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-09 15:20:04 -0600

    Summary required

--- Practice/Title Generator/index.js
+++ Practice/Title Generator/index.js
@@ -3,8 +3,8 @@ const spanA = document.getElementById("A");
 const spanB= document.getElementById("B");
 
 //======================================================================================================================
-const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "Software Development", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce"];
-const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Fuck", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar"]
+const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "Software Development", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce", "Industry"];
+const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Fuck", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder"]
 //======================================================================================================================
 
 button.addEventListener("click", function(){
@@ -15,7 +15,5 @@ button.addEventListener("click", function(){
     //Update the spans
     spanA.textContent = randomNoun;
     spanB.textContent = randomTitle;
-    
-
     console.log("Your new title is... " + randomNoun +" "+ randomTitle);
 });
