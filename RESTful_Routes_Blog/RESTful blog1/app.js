ca129104e5ab1c9f00245778c83866bb5354f103
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-10 15:27:42 -0600

    Blog push

---
+++ RESTful_Routes_Blog/RESTful blog1/app.js
@@ -0,0 +1,66 @@
+const express   = require("express"),
+bodyParser      = require("body-parser"),
+mongoose        = require("mongoose"),
+app             = express();
+
+//Set up mongoose and host it in a restful_blog_app
+mongoose.connect("mongodb://localhost/restful_blog_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Sets the view engine to ejs/App config
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+//MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful Routes
+app.get("/", function(req, res){
+    res.redirect("/blogs")
+});
+
+//Index Route
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+//New Route
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+//Create Route
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+        } else {
+            //Then redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log("=========================================");
+    console.log("Server is listening on port 3000");
+    console.log("=========================================");
+});
