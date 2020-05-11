4aa8671a855603eb8ed3c0651ba2a63bcc6ef4d5
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-11 06:39:25 -0600

    Update to CSS

--- Practice/Title_Generator/index.js
+++ Practice/Title_Generator/index.js
@@ -4,7 +4,7 @@ const spanB= document.getElementById("B");
 
 //======================================================================================================================
 const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce", "Industry", "Chief"];
-const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder", "Priest", "Rogue", "Receptionist Officer", "Officer"];
+const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder", "Priest", "Rogue", "Receptionist Officer", "Officer", "At Large", "At Law", "PhD"];
 //======================================================================================================================
 
 button.addEventListener("click", function(){
