cc7456eb6d2f567e1262ea7159a3caed6175cc31
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-10 22:07:12 -0600

    Updates to style and code

--- Practice/Title_Generator/index.js
+++ Practice/Title_Generator/index.js
@@ -3,8 +3,8 @@ const spanA = document.getElementById("A");
 const spanB= document.getElementById("B");
 
 //======================================================================================================================
-const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "Software Development", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce", "Industry"];
-const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder", "Priest", "Rogue"];
+const noun = ["Sales", "Marketing", "HTML", "JavaScript", "Accounting", "IT", "Recruitng", "Human Resources", "People", "Software", "Charity", "Underling", "Brand", "Graphic", "Social Media", "Media Relations", "eCommerce", "Industry", "Chief"];
+const title = ["Engineer", "Specialist", "Associate", "Analyst", "Advocate", "Student", "Champion", "Founder", "Executive", "Manager", "Agent", "Coder", "Programmer", "Hacker", "Priest", "High Priest", "Shaman", "Sales Manager", "Account Executive", "Mancer", "Head", "Lead", "Moron", "Developer", "Buyer", "Assistant", "Copywriter", "Coordinator", "Hero", "Programmer", "Journalist", "Line Cook", "Cashier", "Player", "Chief", "Warrior", "Lich", "Ninja", "Rockstar", "Paladin", "Scaler", "Builder", "Priest", "Rogue", "Receptionist Officer", "Officer"];
 //======================================================================================================================
 
 button.addEventListener("click", function(){
