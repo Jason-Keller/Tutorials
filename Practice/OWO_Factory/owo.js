ed305d32da299cf64d4236333c92001f92abaf08
Jason Keller <31715620+Jason-Keller@users.noreply.github.com>
2020-05-10 20:08:38 -0600

    Update

--- Practice/OWO_Factory/owo.js
+++ Practice/OWO_Factory/owo.js
@@ -2,22 +2,6 @@ const inp = document.querySelector("input");
 const txtarea = document.getElementById("txt");
 const txtarea2 = document.getElementById("txt2");
 
-// inp.addEventListener("keydown", function(){
-//     const keyPress = event.keyCode;
-//     const inpValue = inp.value;
-//     if(keyPress === 13){
-//         console.log(inpValue.split(""));
-//         this.value = "";
-//         for(var i = 0; i < inpValue.length; i++){
-//             if(inpValue[i] === "r"){
-//                 const regex = /r/gi;
-//                 console.log("Found an r! Replacing...");
-//                 alert(inpValue.replace(regex, "w"));
-//             }
-//         }
-//     }
-// });
-
 txtarea.addEventListener("keydown", function(){
     const keyPress = event.keyCode;
     const inpValue = txtarea.value;
